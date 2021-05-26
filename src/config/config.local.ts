a27dea75b42ddcd03a98ee825ba1a4877a67e208
Yvan <yvan_chen@qq.com>
2021-05-26 22:13:45 +0800

    去除本地配置

--- src/config/config.local.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-  nacosClient: { serverAddr: '47.115.53.158:8848', username: 'nacos', password: 'nacos', namespace: 'public', dataId: 'midwayjs', group: 'DEFAULT_GROUP' },
-  env: process.env.env || 'local',
-};
-export default config;
