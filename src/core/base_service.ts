97a144ef1939a5a2fb5798d1f81288339faff0b6
Yvan <yvan_chen@qq.com>
2021-06-03 09:33:53 +0800

    添加类型

--- src/core/base_service.ts
+++ src/core/base_service.ts
@@ -1,33 +1,40 @@
 import { AnyParamConstructor, DocumentType, ReturnModelType } from '@typegoose/typegoose/lib/types';
-import { PaginateModel, FilterQuery, ModelUpdateOptions, QueryFindOptions, PaginateOptions, PaginateResult, UpdateQuery } from 'mongoose';
+import {
+  CreateQuery,
+  FilterQuery,
+  PaginateModel,
+  PaginateOptions,
+  PaginateResult,
+  QueryFindBaseOptions,
+  QueryFindOneAndUpdateOptions,
+  QueryFindOptions,
+  UpdateQuery,
+} from 'mongoose';
 export class BaseService<T> {
   protected model: PaginateModel<DocumentType<T>> & ReturnModelType<AnyParamConstructor<T>>;
   async findAllWithPaginate(filter: FilterQuery<DocumentType<T>>, options?: PaginateOptions): Promise<PaginateResult<DocumentType<T>>> {
     const models = await this.model.paginate(filter, options);
     return models;
   }
-  async findAll(filter: FilterQuery<DocumentType<T>>, projection?, options?: QueryFindOptions): Promise<DocumentType<T>[]> {
+  async findAll(filter: FilterQuery<DocumentType<T>>, projection = null, options?: QueryFindOptions): Promise<DocumentType<T>[]> {
     const models = await this.model.find(filter, projection, options);
     return models;
   }
-  async findOne(filter: FilterQuery<DocumentType<T>>): Promise<DocumentType<T>> {
-    const model = await this.model.findOne(filter);
+  async findOne(filter: FilterQuery<DocumentType<T>>, projection = null, options?: QueryFindBaseOptions): Promise<DocumentType<T>> {
+    const model = await this.model.findOne(filter, projection, options);
     return model;
   }
-  async findById(id: string): Promise<DocumentType<T>> {
-    const model = await this.model.findById(id);
+  async findById(id: string, projection = null, options?: QueryFindBaseOptions): Promise<DocumentType<T>> {
+    const model = await this.model.findById(id, projection, options);
     return model;
   }
-  async create(data): Promise<DocumentType<T>> {
+  async create(data: CreateQuery<DocumentType<T>>): Promise<DocumentType<T>> {
     const model = await this.model.create(data);
     return model;
   }
-  async update(filter: FilterQuery<DocumentType<T>>, data: UpdateQuery<DocumentType<T>>, options: ModelUpdateOptions): Promise<DocumentType<T>> {
-    const model = await this.model.updateMany(filter, data, options);
-    return model;
-  }
-  async updateById(id: string, data): Promise<DocumentType<T>> {
-    const model = await this.model.findByIdAndUpdate(id, data, { new: true });
+  async updateById(id: string, update: UpdateQuery<DocumentType<T>>, options: QueryFindOneAndUpdateOptions): Promise<DocumentType<T>> {
+    options.new = true;
+    const model = await this.model.findByIdAndUpdate(id, update, options);
     return model;
   }
   async deleteById(id: string): Promise<DocumentType<T>> {
