9d241d7d83dfd766b10c2e330ad8f43338748f7c
Yvan <yvan_chen@qq.com>
2021-12-06 23:31:17 +0800

    不同步本地配置

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,4 +1,18 @@
 const config = {
   env: process.env.env || 'prod',
+  nacosClient: { serverAddr: 'localhost:8848', username: 'nacos', password: '123456', namespace: 'public', dataId: 'prod.json', group: 'DEFAULT_GROUP' },
+  // task: { prefix: 'task', defaultJobOptions: { repeat: { tz: 'Asia/Shanghai' } }, redis: { host: 'localhost', port: 6379, password: '', db: 0 } },
+  // redis: {
+  //   clients: {
+  //     session: { host: 'localhost', port: 6379, password: '', db: 1 },
+  //     cache: { host: 'localhost', port: 6379, password: '', db: 2 },
+  //   },
+  // },
+  // mongoose: {
+  //   clients: {
+  //     default: { uri: 'mongodb://test:123456@localhost/test', options: { useNewUrlParser: true, useUnifiedTopology: true } },
+  //     log: { uri: 'mongodb://test:123456@localhost/test', options: { useNewUrlParser: true, useUnifiedTopology: true } },
+  //   },
+  // },
 };
 export default config;
